6276e7a96bd0a04191f2af0650cfef4b4e0e4574
GnolizX@gmail.com <GnolizX@gmail.com>
2020-06-28 11:12:49 +0800

    Login and register

--- app/src/main/java/com/example/fingertiptime/RegisterActivity.java
+++ app/src/main/java/com/example/fingertiptime/RegisterActivity.java
@@ -1,6 +1,9 @@
 package com.example.fingertiptime;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,24 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button regBtn = (Button) findViewById(R.id.regConfBtn);
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText nameEditText = (EditText)findViewById(R.id.editTextTextPersonName);
+                EditText pwdEditText = (EditText)findViewById(R.id.editTextTextPassword);
+
+                String username = nameEditText.getText().toString();
+                String password = pwdEditText.getText().toString();
+
+                // TODO 写入数据库
+
+                /*
+                 * 注册成功后返回
+                 */
+                finish();
+            }
+        });
     }
 }
